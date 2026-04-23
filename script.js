778f71c4d8636e71a3f7e2d5c171603eaf8dcee2
deepak1218-26 <deeancpahikta1218@gmail.com>
2026-04-23 14:19:20 +0530

    Final ML integration

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
-function startModel() {
-    alert("Model started!");
+const URL = "https://teachablemachine.withgoogle.com/models/7gOpFRAmf/";
+
+let model, webcam, labelContainer, maxPredictions;
+
+async function startModel() {
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
+
+    // Load model
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+
+    // Setup webcam
+    webcam = new tmImage.Webcam(200, 200, true);
+    await webcam.setup();
+    await webcam.play();
+    window.requestAnimationFrame(loop);
+
+    // Add webcam to page
+    document.getElementById("webcam-container").appendChild(webcam.canvas);
+
+    // Labels
+    labelContainer = document.getElementById("label-container");
+    for (let i = 0; i < maxPredictions; i++) {
+        labelContainer.appendChild(document.createElement("div"));
+    }
+}
+
+async function loop() {
+    webcam.update();
+    await predict();
+    window.requestAnimationFrame(loop);
+}
+
+async function predict() {
+    const prediction = await model.predict(webcam.canvas);
+    for (let i = 0; i < maxPredictions; i++) {
+        const classPrediction =
+            prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+        labelContainer.childNodes[i].innerHTML = classPrediction;
+    }
 }
